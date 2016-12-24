1b8c80edfe9c271cc83388ab576dd4584449b35f
pmadruga <pmadruga@gmail.com>
2016-12-24 19:55:31 +0000

    feature: bin path (plus linting)

--- source/index.js
+++ source/index.js
@@ -1,48 +1,38 @@
-'use strict';
-
 const spawn = require('child_process').spawn;
 
 const watchman = spawn('watchman', ['watch-del-all']);
-const wipeNodeModules = spawn('rm', ['-rf', 'node_modules', '-rf', '$TMPDIR/react-*'])
+const wipeNodeModules = spawn('rm', ['-rf', 'node_modules', '-rf', '$TMPDIR/react-*']);
 const cleanCache = spawn('npm', ['cache', 'clean']);
 const brewUpdate = spawn('brew', ['update']);
 const installNodeModules = spawn('yarn');
 
-const runTasks = [
-	{ 
-		task: watchman, 
-		name: 'watchman' 
-	}, 
-	{ 
-		task: wipeNodeModules, 
-		name: 'wipeNodeModules' 
-	},
-	{ 
-		task: cleanCache, 
-		name: 'cleanCache' 
-	},
-	{ 
-		task: brewUpdate, 
-		name: 'brewUpdate' 
-	},
-	{ 
-		task: installNodeModules, 
-		name: 'installNodeModules' 
-	}
-];
+const runTasks = [{
+    task: watchman,
+    name: 'watchman'
+}, {
+    task: wipeNodeModules,
+    name: 'wipeNodeModules'
+}, {
+    task: cleanCache,
+    name: 'cleanCache'
+}, {
+    task: brewUpdate,
+    name: 'brewUpdate'
+}, {
+    task: installNodeModules,
+    name: 'installNodeModules'
+}];
 
-runTasks.map((currentValue, index) => {
-
+runTasks.map((currentValue) => {
     currentValue.task.stderr.on('data', data => {
         console.log(`Error running ${currentValue.name}: ${data}`);
     });
 
     currentValue.task.on('exit', code => {
-    	
-        if(currentValue.name === 'brewUpdate' && code === 0){
-        	spawn('brew', ['upgrade']);
+        if (currentValue.name === 'brewUpdate' && code === 0) {
+            spawn('brew', ['upgrade']);
         }
 
         console.log(`✅  ${currentValue.name} task has finished running`);
     });
-})
+});
