036fab254e6356fffbe2470223674f1213a17298
Cody Brouwers <clbrouwers@gmail.com>
2018-03-04 21:15:17 -0500

    Update how tasks are executed and add additional tasks

--- README.md
+++ README.md
@@ -1,33 +1,36 @@
 # React Native Clean Project
+
 Cleans your React Native project by purging caches and modules, and reinstalling them again.
 
 # Install
+
 `yarn add -D react-native-clean-project`
 
 # Run
+
 `./node_modules/.bin/react-native-clean-project`
 
 # Content
- This is a combination of the commands suggested in the React Native documentation plus others. They are:
 
- 1. `watchman watch-del-all`
- 2. `rm -rf node_modules`
- 3. `rm -rf $TMPDIR/react-*`
- 4. `yarn cache clean`
- 5. `brew update`
- 6. `brew upgrade`
- 7. `yarn`
+This is a combination of the commands suggested in the React Native documentation plus others. They are:
+
+1. `rm -rf ios/build` (optional)
+2. `rm -rf android/build` (optional)
+3. `watchman watch-del-all`
+4. `rm -rf $TMPDIR/react-*`
+5. `rm -rf $TMPDIR/metro-*`
+6. `brew update`
+7. `brew upgrade`
+8. `rm -rf node_modules` (optional)
+9. `yarn cache clean`
+10. `yarn install`
 
 # Tip
-Add to package.json
-`
-"scripts": {
-	...
-    "purge": "./node_modules/.bin/react-native-clean-project",
-	...
-  },
-  `
 
-# 
+You can also reset the Metro bundler cache when starting with `react-native start --reset-cache`
+Add to package.json
+`"scripts": { ... "purge": "./node_modules/.bin/react-native-clean-project", ... },`
+
+#
 
 PR's welcome
--- source/index.js
+++ source/index.js
@@ -1,49 +1,129 @@
 #!/usr/bin/env node
-const spawn = require('child_process').spawn;
-const watchman = spawn('watchman', ['watch-del-all']);
-const wipeNodeModules = spawn('rm', [
-    '-rf',
-    'node_modules',
-    '-rf',
-    '$TMPDIR/react-*'
-]);
-const cleanCache = spawn('yarn', ['cache', 'clean']);
-const brewUpdate = spawn('brew', ['update']);
-const installNodeModules = spawn('yarn');
+/* eslint-disable no-console */
 
-const runTasks = [
-    {
-        task: watchman,
-        name: 'watchman'
-    },
-    {
-        task: wipeNodeModules,
-        name: 'wipeNodeModules'
-    },
-    {
-        task: cleanCache,
-        name: 'cleanCache'
-    },
-    {
-        task: brewUpdate,
-        name: 'brewUpdate'
-    },
-    {
-        task: installNodeModules,
-        name: 'installNodeModules'
+const { spawn } = require('child_process');
+const { createInterface } = require('readline');
+
+const tasksList = {
+  wipeiOSBuildFolder: {
+    name: 'wipe iOS build folder',
+    command: 'rm',
+    args: ['-rf', 'ios/build']
+  },
+  wipeAndroidBuildFolder: {
+    name: 'wipe android build folder',
+    command: 'rm',
+    args: ['-rf', 'android/build']
+  },
+  watchmanCacheClear: {
+    name: 'watchman cache clear',
+    command: 'watchman',
+    args: ['watch-del-all']
+  },
+  wipeTempCaches: {
+    name: 'wipe temporary caches',
+    command: 'rm',
+    args: ['-rf', '$TMPDIR/react-*', '$TMPDIR/metro-*']
+  },
+  brewUpdate: {
+    name: 'brew update',
+    command: 'brew',
+    args: ['update']
+  },
+  brewUpgrade: {
+    name: 'brew upgrade',
+    command: 'brew',
+    args: ['upgrade']
+  },
+  wipeNodeModules: {
+    name: 'wipe node_modules',
+    command: 'rm',
+    args: ['-rf', 'node_modules', '$TMPDIR/react-*', '$TMPDIR/metro-*']
+  },
+  yarnCacheClean: {
+    name: 'yarn cache clean',
+    command: 'yarn',
+    args: ['cache', 'clean']
+  },
+  yarnInstall: {
+    name: 'yarn install',
+    command: 'yarn',
+    args: ['install']
+  }
+};
+
+function elapsedTime (startTime) {
+  const precision = 0;
+  const elapsed = process.hrtime(startTime)[1] / 1000000;
+  const secondCount = process.hrtime(startTime)[0];
+  const millisecondCount = elapsed.toFixed(precision);
+
+  if (secondCount > 0) return `${secondCount}s`;
+  return `${millisecondCount}ms`;
+}
+
+function executeTask (task) {
+  return new Promise(resolve => {
+    const startTime = process.hrtime();
+    const spawnedTask = spawn(task.command, task.args);
+
+    spawnedTask.stderr.on('data', data => {
+      console.log(`Error running ${task.name}: ${data}`);
+    });
+
+    spawnedTask.on('exit', code => {
+      if (code !== 0) {
+        console.log(`Command execution failed with code: ${code}`);
+      } else {
+        console.log(
+          `✅  ${task.name} task has finished running in ${elapsedTime(
+            startTime
+          )}.`
+        );
+      }
+      resolve(code);
+    });
+  });
+}
+
+const rlInterface = createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let wipeiOSBuild;
+let wipeAndroidBuild;
+let wipeNodeModules;
+
+rlInterface.question('Wipe iOS build folder? (Y/n) ', iosAnswer => {
+  if (iosAnswer === 'Y') wipeiOSBuild = true;
+
+  rlInterface.question('Wipe android build folder? (Y/n) ', androidAnswer => {
+    if (androidAnswer === 'Y') wipeAndroidBuild = true;
+
+    rlInterface.question(
+      'Wipe node_modules folder? (Y/n) ',
+      nodeModulesAnswer => {
+        if (nodeModulesAnswer === 'Y') wipeNodeModules = true;
+        rlInterface.close();
+      }
+    );
+  });
+});
+
+rlInterface.on('close', () => {
+  if (wipeiOSBuild) executeTask(tasksList.wipeiOSBuildFolder);
+  if (wipeAndroidBuild) executeTask(tasksList.wipeAndroidBuildFolder);
+  executeTask(tasksList.watchmanCacheClear);
+  executeTask(tasksList.wipeTempCaches);
+  executeTask(tasksList.brewUpdate).then(code => {
+    if (code === 0) {
+      executeTask(tasksList.brewUpgrade);
     }
-];
-
-runTasks.map(currentValue => {
-    currentValue.task.stderr.on('data', data => {
-        console.log(`Error running ${currentValue.name}: ${data}`);
-    });
-
-    currentValue.task.on('exit', code => {
-        if (currentValue.name === 'brewUpdate' && code === 0) {
-            spawn('brew', ['upgrade']);
-        }
-
-        console.log(`✅  ${currentValue.name} task has finished running`);
-    });
+  });
+  if (wipeNodeModules) {
+    executeTask(tasksList.wipeNodeModules)
+      .then(() => executeTask(tasksList.yarnCacheClean))
+      .then(() => executeTask(tasksList.yarnInstall));
+  }
 });
