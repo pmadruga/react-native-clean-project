d8de7a3aaf5ca4660fcbf5183772aca16a897ce5
Pedro Madruga <pedro@occam.ooo>
2019-11-25 17:26:26 +0100

    tests: testing for tasks number increase

--- test/tasks.test.js
+++ test/tasks.test.js
@@ -14,7 +14,7 @@ const plugin = require('../source/plugin');
 describe('Tasks List', () => {
   it('should have twelve tasks in total', () => {
     const input = Object.keys(tasks).length;
-    const expected = 12;
+    const expected = 13;
 
     expect(input).toEqual(expected);
   });
@@ -24,7 +24,7 @@ describe('Correct auto tasks run', () => {
   it('should run the correct tasks in plugin auto-clean mode', () => {
     // auto-mode is the first plugin function, execute it
     plugin[0].func();
-    expect(tasksExecuted.length).toEqual(8);
+    expect(tasksExecuted.length).toEqual(9);
     autoTasks.forEach(task => {
       expect(tasksExecuted.includes(task.name)).toEqual(true);
     });
