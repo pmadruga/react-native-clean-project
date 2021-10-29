f12f3f7c9c044f0d125216531a39ea9e12276932
Pedro Madruga <hello@pedromadruga.com>
2021-10-29 19:31:03 +0200

    fix: making clean-project-auto tasks async

--- source/plugin.js
+++ source/plugin.js
@@ -18,23 +18,23 @@ module.exports = [
 
       rlInterface.close(); // if we don't do this it hangs waiting for input
 
-      let taskPromises = [];
-      autoTasks.forEach(async (task) => {
-        const result = await executeTask(task);
-        taskPromises.push(result);
-      });
+      async function executeTasks() {
+        for (const task of autoTasks) {
+          await executeTask(task);
+        }
+      }
 
-      return Promise.all(taskPromises).then(() => {
-        console.log('');
-        console.log(
-          'Project cleaned. Use yarn or npm, pod, etc to re-install packages.'
-        );
-        console.log(
-          'You may also want to clean the metro bundler cache. It can only be cleaned on metro startup, like this:'
-        );
-        console.log('react-native start -- --reset-cache');
-        console.log('');
-      });
+      executeTasks();
+
+      console.log('');
+      console.log(
+        'Project cleaned. Use yarn or npm, pod, etc to re-install packages.'
+      );
+      console.log(
+        'You may also want to clean the metro bundler cache. It can only be cleaned on metro startup, like this:'
+      );
+      console.log('react-native start -- --reset-cache');
+      console.log('');
     }
   },
   {
